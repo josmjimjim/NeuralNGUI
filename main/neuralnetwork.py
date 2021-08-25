80c1eafd412c5a472f2d1c36cf394f7cceabdf92
josmjimjim <josmjimjim@gmail.com>
2021-08-25 21:51:29 +0200

    Update neuralnetwork.py

--- main/neuralnetwork.py
+++ main/neuralnetwork.py
@@ -372,8 +372,8 @@ class NeuralNetwork(object):
 
         # Confusion matrix
         cf_matrix = confusion_matrix(y_true, y_pred)
-        df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix) * 100, index = [i for i in range(num_class+1)],
-                     columns = [i for i in range(num_class+1)])
+        df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix) * 100, index = [i for i in range(num_class)],
+                     columns = [i for i in range(num_class)])
 
         plt.figure(figsize=(12, 7))
         sns.heatmap(df_cm, annot=True)
